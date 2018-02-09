5def24bb4d785f9ad0a36c3a6000fcef4c46ee47
jpuigcerver <joapuipe@gmail.com>
2018-02-09 19:53:19 +0100

    Support for Python3

--- pytorch/nnutils_pytorch/__init__.py
+++ pytorch/nnutils_pytorch/__init__.py
@@ -1,4 +1,5 @@
-from nnutils import is_cuda_available, mask_image_from_size, adaptive_avgpool_2d, adaptive_maxpool_2d
+from .nnutils import is_cuda_available
+from .nnutils import mask_image_from_size, adaptive_avgpool_2d, adaptive_maxpool_2d
 
 __all__ = [is_cuda_available,
            mask_image_from_size,
