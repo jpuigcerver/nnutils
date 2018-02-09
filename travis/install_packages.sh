64539fa6346e795ca1f2b3de6676de58b47a4c41
jpuigcerver <joapuipe@gmail.com>
2018-02-09 16:18:10 +0100

    Added cffi dependency for Travis

--- travis/install_packages.sh
+++ travis/install_packages.sh
@@ -8,9 +8,9 @@ elif [ "$TRAVIS_OS_NAME" = osx ]; then
 fi;
 
 if [ "$TRAVIS_PYTHON_VERSION" = "2.7" ]; then
-  pip cffi;
+  pip install cffi;
 elif [ "$TRAVIS_PYTHON_VERSION" = "3.5" ]; then
-  pip3 cffi;
+  pip3 install cffi;
 elif [ "$TRAVIS_PYTHON_VERSION" = "3.6" ]; then
-  pip3 cffi;
+  pip3 install cffi;
 fi;
