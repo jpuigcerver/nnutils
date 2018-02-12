40423f66b429b31bafd6349b5d5093ac6c8251ea
Joan Puigcerver <joapuipe@gmail.com>
2018-02-12 16:25:43 +0100

    Fixing Travis build

--- travis/build.sh
+++ travis/build.sh
@@ -3,6 +3,9 @@ set -e;
 
 mkdir build;
 cd build;
-cmake -DWITH_CUDA=OFF -DWITH_PYTORCH=ON -DCMAKE_BUILD_TYPE=DEBUG \
-      -DPYTORCH_SETUP_PREFIX=$HOME/.local ..;
+cmake -DWITH_CUDA=OFF -DWITH_PYTORCH=ON -DCMAKE_BUILD_TYPE=DEBUG ..;
 make VERBOSE=1;
+
+cd pytorch;
+python setup.py bdist_wheel;
+pip install $(find dist/ -name "*.whl");
--- travis/test.sh
+++ travis/test.sh
@@ -5,9 +5,7 @@ set -e;
 cd build;
 make test;
 
-# Install Python module & test the installation.
-export PYTHONPATH="$HOME/.local/lib/python${TRAVIS_PYTHON_VERSION}/site-packages:$PYTHONPATH";
-make install;
+# Test module installed via pip.
 python -m unittest nnutils_pytorch.mask_image_from_size_test;
 python -m unittest nnutils_pytorch.adaptive_avgpool_2d_test;
 python -m unittest nnutils_pytorch.adaptive_maxpool_2d_test;
