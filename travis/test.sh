2c6239c3fdb586ebbb08d95dd5a7f1bb939b9117
Joan Puigcerver <joapuipe@gmail.com>
2018-02-12 16:18:25 +0100

    Updated travis scripts

--- travis/test.sh
+++ travis/test.sh
@@ -6,6 +6,7 @@ cd build;
 make test;
 
 # Install Python module & test the installation.
+export PYTHONPATH="$HOME/.local/lib/python${TRAVIS_PYTHON_VERSION}/site-packages:$PYTHONPATH";
 make install;
 python -m unittest nnutils_pytorch.mask_image_from_size_test;
 python -m unittest nnutils_pytorch.adaptive_avgpool_2d_test;
