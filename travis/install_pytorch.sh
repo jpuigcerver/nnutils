4ba0b085a38eeb4307f1bca1ababb99c632891b8
Joan Puigcerver <joapuipe@gmail.com>
2018-06-15 20:26:09 +0200

    Install Troch 0.3.1

--- travis/install_pytorch.sh
+++ travis/install_pytorch.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -e;
 
+BASE_URL=http://download.pytorch.org/whl;
+
 if [ "$TRAVIS_OS_NAME" = linux ]; then
   if [ "$TRAVIS_PYTHON_VERSION" = "2.7" ]; then
-    pip install http://download.pytorch.org/whl/cpu/torch-0.3.0.post4-cp27-cp27mu-linux_x86_64.whl;
+    pip install $BASE_URL/cpu/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl;
   elif [ "$TRAVIS_PYTHON_VERSION" = "3.5" ]; then
-    pip3 install http://download.pytorch.org/whl/cpu/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl;
+    pip3 install $BASE_URL/cpu/torch-0.3.1-cp35-cp35m-linux_x86_64.whl;
   elif [ "$TRAVIS_PYTHON_VERSION" = "3.6" ]; then
-    pip3 install http://download.pytorch.org/whl/cpu/torch-0.3.0.post4-cp36-cp36m-linux_x86_64.whl;
+    pip3 install $BASE_URL/cpu/torch-0.3.1-cp36-cp36m-linux_x86_64.whl;
   fi;
 elif [ "$TRAVIS_OS_NAME" = osx ]; then
   if [ "$TRAVIS_PYTHON_VERSION" = "2.7" ]; then
-    pip install http://download.pytorch.org/whl/torch-0.3.0.post4-cp27-none-macosx_10_6_x86_64.whl;
+    pip install $BASE_URL/torch-0.3.1-cp27-none-macosx_10_6_x86_64.whl;
   elif [ "$TRAVIS_PYTHON_VERSION" = "3.5" ]; then
-    pip3 install http://download.pytorch.org/whl/torch-0.3.0.post4-cp35-cp35m-macosx_10_6_x86_64.whl;
+    pip3 install $BASE_URL/torch-0.3.1-cp35-cp35m-macosx_10_6_x86_64.whl;
   elif [ "$TRAVIS_PYTHON_VERSION" = "3.6" ]; then
-    pip3 install http://download.pytorch.org/whl/torch-0.3.0.post4-cp36-cp36m-macosx_10_7_x86_64.whl;
+    pip3 install $BASE_URL/torch-0.3.1-cp36-cp36m-macosx_10_7_x86_64.whl;
   fi;
 fi;
